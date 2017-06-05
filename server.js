a193479f19883df23bcb298e9cb968dc3be43630
Vedran Serenčeš <vedranserences@gmail.com>
2017-06-05 14:59:44 +0200

    api

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var sqlite3 = require('sqlite3').verbose();
+var bodyParser = require('body-parser');
+
+var router = express.Router();
+var app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+var port = process.env.PORT || 8080;        // set our port
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('db.db');
+
+
+router.get('/leaderboard', function (req, res) {
+  db.all('SELECT * FROM player', function (err, row) {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json(row);
+  })
+});
+
+router.post('/game', function (req, res) {
+  console.log('New request received: ')
+  console.log(req.body);
+  var username = req.body.username;
+  var points = req.body.points;
+  var time = req.body.time;
+  db.run("INSERT INTO player VALUES (?, ?,?)", [username, points, time]);
+  res.json("");
+});
+
+// INIT API
+app.use('/', router)
+
+app.listen(port);
